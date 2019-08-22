640d118c42b300dc95d3eb13814eabad11ba220f
smart_an <184480602@qq.com>
2019-08-22 14:14:36 +0800

    you

--- src/main/java/com/zte/ffa/controller/Test1.java
+++ src/main/java/com/zte/ffa/controller/Test1.java
@@ -11,7 +11,7 @@ public class Test1 {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-    System.out.println("xnwenj");
+		System.out.println("xnwenj");
 	}
 
 }
