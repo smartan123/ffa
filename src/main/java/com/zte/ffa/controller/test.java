439c51c0b247e4d023a9c10dab267548f8d7688e
smart_an <184480602@qq.com>
2019-08-22 13:45:30 +0800

    第一次提交

---
+++ src/main/java/com/zte/ffa/controller/test.java
@@ -0,0 +1,19 @@
+package com.zte.ffa.controller;
+
+/**
+ * @author: ShuaiLing
+ * @date: 2019年8月22日
+ */
+public class test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("sdfdsfF");
+		System.out.println("sdfdsfF");
+
+	}
+
+}
