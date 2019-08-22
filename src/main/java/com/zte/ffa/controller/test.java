a10bacc00c210670ed5cdc20363c3a2c7249453c
smart_an <184480602@qq.com>
2019-08-22 13:54:04 +0800

    第二三次修改

--- src/main/java/com/zte/ffa/controller/test.java
+++ src/main/java/com/zte/ffa/controller/test.java
@@ -12,8 +12,8 @@ public class test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("sdfdsfF");
-		System.out.println("sdfdsfF");
-
+		System.out.println("这是第二次修改");
+		System.out.println("这是第三次修改");
 	}
 
 }
